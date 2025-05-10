4b6d56780f1ea585458fd37896fbe4b7c394f0c5
Edir <ecaramba@gmail.com>
2025-05-10 16:04:02 -0300

    configura entraid

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,20 @@
 import NextAuth from "next-auth"
 import MicrosoftEntraID from "next-auth/providers/microsoft-entra-id"
  
+const { 
+  AUTH_MICROSOFT_ENTRA_ID_ID, 
+  AUTH_MICROSOFT_ENTRA_ID_SECRET, 
+  AUTH_MICROSOFT_ENTRA_ID_ISSUER } = process.env;
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
     MicrosoftEntraID({
-      clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID,
-      clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
-      issuer: process.env.AUTH_MICROSOFT_ENTRA_ID_ISSUER,
+      clientId: AUTH_MICROSOFT_ENTRA_ID_ID,
+      clientSecret: AUTH_MICROSOFT_ENTRA_ID_SECRET,
+      issuer: `https://login.microsoftonline.com/${AUTH_MICROSOFT_ENTRA_ID_ISSUER}/v2.0`,
+      // issuer: process.env.AUTH_MICROSOFT_ENTRA_ID_ISSUER,
+      authorization: `https://login.microsoftonline.com/${AUTH_MICROSOFT_ENTRA_ID_ISSUER}/oauth2/v2.0/authorize`,
+			token: `https://login.microsoftonline.com/${AUTH_MICROSOFT_ENTRA_ID_ISSUER}/oauth2/v2.0/token`,
     }),
   ],
 })
